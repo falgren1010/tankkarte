a0c6f3ea260ebb548985346e265987c91803568e
falgren1010 <lu.weber97@web.de>
2022-02-24 15:25:20 +0100

    implemented classes

---
+++ src/main/java/EncryptionDES.java
@@ -0,0 +1,40 @@
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptionDES implements ICryptionStrategy {
+
+    private SecretKeySpec secretKey;
+    private byte[] key;
+
+    public void setKey(final String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String encrypt(final String strToEncrypt, final String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher =  Cipher.getInstance("DES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder()
+                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/Silver.java
@@ -0,0 +1,18 @@
+public class Silver extends CardState{
+
+    public Silver(Card card){
+        super(card);
+    }
+
+    public void addPoints(Integer liters) {
+        this.card.setPoints(this.card.getPoints() + ((liters % 25) * 5) );
+        this.checkForCardStatusUpgrade();
+    }
+
+    @Override
+    protected void checkForCardStatusUpgrade() {
+        if(this.card.getPoints() >= 5000){
+            this.card.setState(new Gold(this.card));
+        }
+    }
+}
